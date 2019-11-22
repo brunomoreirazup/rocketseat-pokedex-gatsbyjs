e4b818062d21bb62f174e087eb1aa832ed24ac04
bruno <bruno.moreira@zup.com.br>
2019-11-22 20:17:34 -0300

    creating project

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react"
+
+const IndexPage = () => (
+  <Fragment>
+    <h1>Hello Rocketseat Experience</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Let's go build a pokédex.</p>
+  </Fragment>
+)
+
+export default IndexPage
